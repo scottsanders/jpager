7d24ccc41be3913d887c8231e982eda3408dfd0c
Murray Bunton <murray@streetlinemedia.com>
2012-08-16 12:25:48 +1000

    class names are now persistent

--- js/jpager.js
+++ js/jpager.js
@@ -4,23 +4,24 @@
     var opts = $.extend({}, $.fn.jpager.defaults, options);
     var count = $(this).children().size();
     var pages = Math.ceil(count / opts.limit);
-    var type = "UL";
+    var type = "ul";
 
     return this.each(function() {
 		
 		type = this.nodeName;
 
 		$(this).wrap("<div class=\"jpager\">");
+		
 		for(i=1;i<=pages;i++){
-			$(this).parent().append("<"+type+" class=\"page-"+i+"\">");
+			
+			$(this).parent().append("<"+type+" class=\""+$(this).parent().attr("class")+" page page-"+i+"\">");
+		
 		}
 
 		$(this).children().each(function(i){
 			
 			$(this).appendTo(type+".page-"+Math.ceil((i+1) / opts.limit));
-			//if((i+1) % Math.ceil(opts.limit === 0){
-				//$(this).after("</ul>");
-			//}
+
 		});
 		
 		$(this).remove();
@@ -29,4 +30,5 @@
   };
 
   $.fn.jpager.defaults = {limit: 4};
+
 })(jQuery);
