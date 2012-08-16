572c8891d3ea622a5edfbb4b8c3acd1d4150420f
Murray Bunton <murray@streetlinemedia.com>
2012-08-16 12:53:02 +1000

    added pagination

--- js/jpager.js
+++ js/jpager.js
@@ -11,20 +11,38 @@
 		type = this.nodeName;
 
 		$(this).wrap("<div class=\"jpager\">");
-		
-		for(i=1;i<=pages;i++){
+
+		for(i=2;i<=pages;i++){
 			
-			$(this).parent().append("<"+type+" class=\""+$(this).parent().attr("class")+" page page-"+i+"\">");
+			$(this).parent().append("<"+type+" class=\""+$(this).attr("class")+" jpage jpage-"+i+"\">");
 		
 		}
 
+		$(this).addClass("jpage jpage-1 jpage-current");
+
 		$(this).children().each(function(i){
 			
-			$(this).appendTo(type+".page-"+Math.ceil((i+1) / opts.limit));
+			$(this).appendTo(type+".jpage-"+Math.ceil((i+1) / opts.limit));
 
 		});
+
+		pagination = "<ul class=\"jpagination\">";
+		//pagination += "<li>&laquo;</li>";
+		for(i=1;i<=pages;i++){
+			pagination += "<li><a class=\"jpaginate\" rel=\"jpage-"+i+"\" href=\"#jpage-"+i+"\">"+i+"</a></li>";
+		}
+		//pagination += "<li>&raquo;</li>";
+		pagination += "</ul>";
+		$(".jpager").append(pagination);
+		$(".jpager").prepend(pagination);
 		
-		$(this).remove();
+		$(".jpaginate").click(function(){
+			$(".jpaginate").removeClass("jpaginate-current");
+			$(".jpage").removeClass("jpage-current");
+			$(this).addClass("jpaginate-current");
+			$("."+$(this).attr("rel")).addClass("jpage-current");
+			return false;
+		});
 	});
 
   };
